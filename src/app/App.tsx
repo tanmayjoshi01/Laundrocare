f680b7e00b1f802e1ecd8471ec8d4d19a04bb316
tanmayjoshi01 <tanmay.joshi2506@gmail.com>
2026-03-31 12:24:44 +0530

    feat: integrate Vercel Speed Insights and initialize App component with providers

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,14 @@ import { RouterProvider } from 'react-router';
 import { router } from './routes';
 import { StoreProvider } from './store';
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function App() {
   return (
     <StoreProvider>
       <RouterProvider router={router} />
       <Analytics />
+      <SpeedInsights />
     </StoreProvider>
   );
 }
