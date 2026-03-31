b823368dcfcef5cf87865424632463ef8b72e2ee
tanmayjoshi01 <tanmay.joshi2506@gmail.com>
2026-03-31 12:13:57 +0530

    feat: integrate Vercel Analytics into the main application component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
 import { StoreProvider } from './store';
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <StoreProvider>
       <RouterProvider router={router} />
+      <Analytics />
     </StoreProvider>
   );
 }
